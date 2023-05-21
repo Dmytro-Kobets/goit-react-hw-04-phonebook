abc0064b33acb3c753dd3a64f992576e0d33d7e5
Dmytro-Kobets <kobets.dmytro9@gmail.com>
2023-05-21 13:45:54 +0300

    fix

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,27 +1,24 @@
-import { Component } from 'react';
 import { DeleteButton } from './App.styled';
 import PropTypes, { objectOf, string } from 'prop-types';
-export class ContactList extends Component {
-  render() {
-    const { contacts, filter, handleDelete } = this.props;
-    return (
-      <ul>
-        {contacts &&
-          contacts.map(contact => {
-            return !filter === '' ||
-              contact.name.toLowerCase().includes(filter.toLowerCase()) ? (
-              <li key={contact.id}>
-                {contact.name}: {contact.number}
-                <DeleteButton onClick={handleDelete} id={contact.id}>
-                  Delete
-                </DeleteButton>
-              </li>
-            ) : null;
-          })}
-      </ul>
-    );
-  }
-}
+
+export const ContactList = ({ contacts, filter, handleDelete }) => {
+  return (
+    <ul>
+      {contacts &&
+        contacts.map(contact => {
+          return !filter === '' ||
+            contact.name.toLowerCase().includes(filter.toLowerCase()) ? (
+            <li key={contact.id}>
+              {contact.name}: {contact.number}
+              <DeleteButton onClick={handleDelete} id={contact.id}>
+                Delete
+              </DeleteButton>
+            </li>
+          ) : null;
+        })}
+    </ul>
+  );
+};
 
 ContactList.propTypes = {
   contacts: PropTypes.arrayOf(objectOf(string)),
